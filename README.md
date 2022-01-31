488d2cdd7195db92239a9c29b8cd86556b26389d
anav <anav2096@gmail.com>
2022-01-30 19:22:00 -0500

    Update Read Me for submission

--- README.md
+++ README.md
@@ -2,11 +2,9 @@
 This online day scheduler accepts task inputs from the user, then displays them as a To-Do list. Task display is time dependent and will change as the deadline approaches. Task content blocks are open for edit and are stored for future use.
 
 
-The user inputs tasks here:
+The user inputs daily tasks into a text field, as time passes the input fields change color tp alert the user that a task dealine is approaching.
+!{ScreenShot](./UI.png)
 
+Visit the deployed app here (https://anav2096.github.io/OnlineDayScheduler/)
 
-The task list looks like this:
-
-Visit the deployed app here
-
-The repository for this project can be found at
+The repository for this project can be found at (git@github.com:anav2096/OnlineDayScheduler.git)
--- asset/js/script.js
+++ asset/js/script.js
@@ -5,6 +5,8 @@
 
  var hour = "";
 
+ console.log(moment().format('H')); 
+
 for (i=0; i<10; i++) {
 
     $(".container").append($(`<div id="row-0${i+1}" class="row time"><div id="time-0${i+1}" class="col-md-1 col-sm-1 col-2 hour"></div><textarea id="text-0${i+1}" class="col-md-10 col-sm-8 col-8"></textarea><button class="col-md-1 col-sm-1 col-sm-1 col-2 saveBtn"><i class="far fa-save"></i></button></div>`))
@@ -21,11 +23,23 @@ for (var index = 8; index < 17; index++) {
     }
 }
 
-if (index == moment().format('H')) {                                                
-    $('time-0${i+1}').eq(index - 8).addClass('present')
-} else if (index < moment().format('H')) {
-    $('time-0${i+1}').eq(index - 8).addClass('past')
-} else {
-    $('time-0${i+1}').eq(index - 8).addClass('future')
-}
+function updateColor(){
+    var hour = new Date().getHours();
+    for (var i= 8 ; i<=18; i++){
+      console.log(hour,i)
+      if(hour==i ) {
+       $('textarea').addClass("present")
+      }else  if(hour<i ){
+       
+        $('textarea').addClass("past")
+
+      }else if (hour>i){
+       $('textarea').addClass("future")
+    }
+  }}
+  setInterval(function(){
+    updateColor()
+  },1000)
+
+
 
